e962186d34d6a7da7443f1a4d9c1c8f4a0b5df59
Twyll <twyllblackleaf@gmail.com>
2021-10-08 17:44:46 -0400

    basic workings of login form

--- server.js
+++ server.js
@@ -11,12 +11,14 @@ const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "public")));
 
 app.use(routes);
-app.use(express.static(path.join(__dirname, "public")));
 
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
+
+app.use(express.static(path.join(__dirname, "public")));
 
 sequelize.sync({ force: false }).then(() => {
     app.listen(PORT, () => console.log("Now listening"));
