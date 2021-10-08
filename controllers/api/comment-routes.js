e10d95d2c601551caac58bf72344eb51ad20adfb
Twyll <twyllblackleaf@gmail.com>
2021-10-08 19:31:09 -0400

    conditional logic in templates

--- controllers/api/comment-routes.js
+++ controllers/api/comment-routes.js
@@ -16,16 +16,19 @@ router.get('/', (req, res) => {
 
 // make a comment
 router.post('/', (req, res) => {
-    Comment.create({
-        comment_text: req.body.comment_text,
-        user_id: req.body.user_id,
-        post_id: req.body.post_id
-    })
-        .then(dbCommentData => res.json(dbCommentData))
-        .catch(err => {
-            console.log(err);
-            res.status(400).json(err);
-        });
+    if (req.session) {
+        Comment.create({
+            comment_text: req.body.comment_text,
+            user_id: req.session.user_id,
+            post_id: req.body.post_id
+        })
+            .then(dbCommentData => res.json(dbCommentData))
+            .catch(err => {
+                console.log(err);
+                res.status(400).json(err);
+            });
+    }
+    
 });
 
 //delete a comment
