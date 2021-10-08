da93d749e8399032a17e092cc987778dac1bd17b
Twyll <twyllblackleaf@gmail.com>
2021-10-08 16:29:36 -0400

    create routes for comments

---
+++ routes/api/comment-routes.js
@@ -0,0 +1,51 @@
+const router = require('express').Router();
+const { Comment } = require('../../models');
+
+// get all comments
+router.get('/', (req, res) => {
+    Comment.findAll({
+        attributes: ['id', 'comment_text', 'user_id', 'post_id', "created_at"],
+        order: [["created_at", "DESC"]],
+    })
+        .then(dbPostData => res.json(dbPostData))
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
+
+// make a comment
+router.post('/', (req, res) => {
+    Comment.create({
+        comment_text: req.body.comment_text,
+        user_id: req.body.user_id,
+        post_id: req.body.post_id
+    })
+        .then(dbCommentData => res.json(dbCommentData))
+        .catch(err => {
+            console.log(err);
+            res.status(400).json(err);
+        });
+});
+
+//delete a comment
+router.delete('/:id', (req, res) => {
+    Comment.destroy({
+        where: {
+            id: req.params.id
+        }
+    })
+        .then(dbPostData => {
+            if (!dbPostData) {
+                res.status(404).json({ message: 'No post found with this id' });
+                return;
+            }
+            res.json(dbPostData);
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
+
+module.exports = router;
