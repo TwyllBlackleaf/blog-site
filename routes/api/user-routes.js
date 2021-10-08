29593e35f81b56671723ac8e4d1f7dd0b4b8a2fd
Twyll <twyllblackleaf@gmail.com>
2021-10-08 14:54:45 -0400

    check hashed password for user login

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -48,6 +48,28 @@ router.post("/", (req, res) => {
         });
 });
 
+// POST login to log user in
+router.post("/login", (req, res) => {
+    User.findOne({
+        where: {
+            email: req.body.email
+        }
+    }).then(dbUserData => {
+        if (!dbUserData) {
+            res.status(400).json({ message: 'No user with that email address!' });
+            return;
+        }
+
+        const validPassword = dbUserData.checkPassword(req.body.password);
+        if (!validPassword) {
+            res.status(400).json({ message: 'Incorrect password!' });
+            return;
+        }
+        
+        res.json({ user: dbUserData, message: 'You are now logged in!' });
+    })
+})
+
 // POST /api/users/:id
 router.post("/:id", (req, res) => {});
 
