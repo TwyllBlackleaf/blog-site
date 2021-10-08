e5a7fec31d7ec05eca22dd4f2671454d7811c69b
Twyll <twyllblackleaf@gmail.com>
2021-10-08 17:58:22 -0400

    cookies and authentication

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -56,7 +56,15 @@ router.post("/", (req, res) => {
         email: req.body.email,
         password: req.body.password
     })
-        .then(userData => res.json(userData))
+        .then(dbUserData => {
+            req.session.save(() => {
+                req.session.user_id = dbUserData.id;
+                req.session.username = dbUserData.username;
+                req.session.loggedIn = true;
+
+                res.json(dbUserData);
+            });
+        })
         .catch(err => {
             console.log(err);
             res.status(500).json(err);
@@ -81,9 +89,28 @@ router.post("/login", (req, res) => {
             return;
         }
 
-        res.json({ user: dbUserData, message: 'You are now logged in!' });
+        req.session.save(() => {
+            // declare session variables
+            req.session.user_id = dbUserData.id;
+            req.session.username = dbUserData.username;
+            req.session.loggedIn = true;
+
+            res.json({ user: dbUserData, message: 'You are now logged in!' });
+        });
     })
 })
+
+// log out user
+router.post('/logout', (req, res) => {
+    if (req.session.loggedIn) {
+        req.session.destroy(() => {
+            res.status(204).end();
+        });
+    }
+    else {
+        res.status(404).end();
+    }
+});
 
 // POST /api/users/:id
 router.post("/:id", (req, res) => { });
